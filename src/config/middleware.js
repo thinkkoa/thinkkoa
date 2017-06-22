326b4f8a78de875cc230520706ddbc369fa4bdcc
yanglin <richenlin@gmail.com>
2017-06-22 19:21:52 +0800

    增加开关控制多模块模式支持

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -47,8 +47,10 @@ module.exports = {
             error_msg_key: 'errmsg', //错误消息的key
         },
         router: {
-            deny_modules: ['common'], //禁止访问的模块
+            mulit_modules: false, //开启多模块支持
+            deny_modules: ['common'], //禁止访问的模块(多模块模式)
             default_module: 'home', //默认的模块
+            deny_controller: [], //禁止访问的控制器
             default_controller: 'index', //默认控制器
             default_action: 'index', //默认方法
             prefix: [], // url prefix
