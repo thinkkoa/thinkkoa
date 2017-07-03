3d7e70082900551dbf677738a40dff71a8f01337
yanglin <richenlin@gmail.com>
2017-07-03 14:56:59 +0800

    增加版本字符v

--- src/util/server.js
+++ src/util/server.js
@@ -48,7 +48,7 @@ module.exports = class {
         lib.logs('====================================', 'THINK');
         lib.logs(`Server running at http://127.0.0.1:${this.options.port}/`, 'THINK');
         lib.logs(`Nodejs Version: ${process.version}`, 'THINK');
-        lib.logs(`ThinkKoa Version: ${think.version}`, 'THINK');
+        lib.logs(`ThinkKoa Version: v${think.version}`, 'THINK');
         lib.logs(`App Enviroment: ${(think.app_debug ? 'debug mode' : 'production mode')}`, 'THINK');
         lib.logs('====================================', 'THINK');
         think.app_debug && lib.logs('Debugging mode is running, if the production environment, please modify the APP_DEBUG value to false', 'WARNING');
