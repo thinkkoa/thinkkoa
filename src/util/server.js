a8d17f1c3d62d618744694aea8d87c248ecdb644
yanglin <richenlin@gmail.com>
2017-07-03 14:54:04 +0800

    移除多余的输出

--- src/util/server.js
+++ src/util/server.js
@@ -47,9 +47,8 @@ module.exports = class {
     started() {
         lib.logs('====================================', 'THINK');
         lib.logs(`Server running at http://127.0.0.1:${this.options.port}/`, 'THINK');
-        lib.logs(`Node.js Version: ${process.version}`, 'THINK');
+        lib.logs(`Nodejs Version: ${process.version}`, 'THINK');
         lib.logs(`ThinkKoa Version: ${think.version}`, 'THINK');
-        lib.logs(`App File Auto Reload: ${(think.app_debug ? 'open' : 'closed')}`, 'THINK');
         lib.logs(`App Enviroment: ${(think.app_debug ? 'debug mode' : 'production mode')}`, 'THINK');
         lib.logs('====================================', 'THINK');
         think.app_debug && lib.logs('Debugging mode is running, if the production environment, please modify the APP_DEBUG value to false', 'WARNING');
