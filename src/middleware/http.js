923ef5ac4793885ad7df6d0175a20f7bb16819b2
yanglin <richenlin@gmail.com>
2017-08-07 18:55:37 +0800

    remove error, http, logger middleware

--- src/middleware/http.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @author     richen
- * @copyright  Copyright (c) 2017 - <richenlin(at)gmail.com>
- * @license    MIT
- * @version    17/4/29
- */
-
-module.exports = require('think_http');
