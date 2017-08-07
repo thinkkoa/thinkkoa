36e702ed3073b223f332cea36b4d426ca3fb80cc
yanglin <richenlin@gmail.com>
2017-08-07 19:02:14 +0800

    add  context, cookie, trace middlewares

---
+++ src/middleware/trace.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @author     richen
+ * @copyright  Copyright (c) 2017 - <richenlin(at)gmail.com>
+ * @license    MIT
+ * @version    17/5/29
+ */
+
+module.exports = require('think_trace');
